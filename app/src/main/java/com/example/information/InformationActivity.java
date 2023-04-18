92e1ef25e6ecbf63f95988f05d9919629b2db877
TaehyunSm <thyj7899@semyung.ac.kr>
2023-04-18 14:12:10 +0900

    //commit 수정

--- app/src/main/java/com/example/information/InformationActivity.java
+++ app/src/main/java/com/example/information/InformationActivity.java
@@ -1,7 +1,59 @@
 package com.example.information;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InformationActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_information);
 
+        Intent it = getIntent();
+        String str = it.getStringExtra("id");
+
+        EditText editTextName = findViewById(R.id.editTextName);
+        EditText editTextAge = findViewById(R.id.editTextAge);
+        RadioButton radioMale = findViewById(R.id.radioMale);
+        RadioButton radioFeMale = findViewById(R.id.radioFemale);
+        CheckBox cBInfo = findViewById(R.id.cBInfo);
+        CheckBox cBAI = findViewById(R.id.cBAI);
+        CheckBox cBSecurity = findViewById(R.id.cBSecurity);
+        Button btnSend = findViewById(R.id.btnSend);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it =new Intent();
+                it.putExtra("name", editTextName.getText().toString());
+                it.putExtra("age", editTextAge.getText().toString());
+                if (radioMale.isChecked() == true)
+                    it.putExtra("sex", "남자");
+                else
+                    it.putExtra("Sex", "여자");
+                String strLicense = "";
+                if (cBInfo.isChecked())
+                    strLicense += "\n 정보처리기사";
+                if (cBAI.isChecked())
+                    strLicense += "\n 인공지능데이터전문가";
+                if (cBSecurity.isChecked())
+                    strLicense += "\n 정보보안기사";
+
+                it.putExtra( "License", strLicense);
+                setResult(RESULT_OK, it);
+                finish();
+
+
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/information/MainActivity.java
+++ app/src/main/java/com/example/information/MainActivity.java
@@ -5,15 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tVResult, tVResultlabel;
+    String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        tVResult = findViewById(R.id.tVReult);
+        tVResultlabel = findViewById(R.id.tVResultlabel);
+        Button btnRequest = findViewById(R.id.btnRequest);
+        Button btnEnd = findViewById(R.id.btnEnd);
+        EditText editTextID = findViewById(R.id.editTextID);
 
+        btnRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                str = editTextID.getText().toString();
+                Intent it = new Intent(getApplicationContext(), InformationActivity.class);
+                it.putExtra("id", str);
+                startActivityForResult(it,1);
+            }
+        });
+
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
 
